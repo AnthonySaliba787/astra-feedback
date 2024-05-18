d88dcfa8fe71c5bfbed5baa66e4eeab0d86e412d
Anthony Saliba <anthonysaliba93@gmail.com>
2024-05-18 16:13:44 +0300

    Add feedback form page with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./components/SignupForm";
 import LoginForm from "./components/LoginForm";
+import FeedbackForm from "./components/FeedbackForm";
 import NoPage from "./components/NoPage";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<SignUp />} />
           <Route exact path="/login" element={<LoginForm />} />
+          <Route exact path="/feedback" element={<FeedbackForm />} />
           <Route path="*" element={<NoPage />} />
         </Routes>
       </HashRouter>
---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,11 @@
+function FeedbackForm() {
+  return (
+    <>
+      <div className="max-w-xl min-h-screen mx-auto text-center flex flex-col justify-center items-center px-8 py-8">
+        <h1 className="text-3xl font-semibold py-4">Feedback Form</h1>
+      </div>
+    </>
+  );
+}
+
+export default FeedbackForm;
